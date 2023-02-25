48e413ad0c418285617733c9c2861b76507eb8b1
SridaranSivakumar <sridaran.sivakumar@cdw.com>
2023-02-25 12:38:10 +0530

    Day 1 Changes

--- DAY 1/Palindrome_15_2.java
+++ DAY 1/Palindrome_15_2.java
@@ -1,5 +1,7 @@
 package Day1Task;
 
+
+
 import java.util.Scanner;
 
 public class Palindrome_15_2 {
@@ -7,60 +9,61 @@ public class Palindrome_15_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       Scanner sc=new Scanner(System.in);
-      int N=sc.nextInt();
-      int PalinNum=Palindrome(N);
-      int AmsNum=Amstrong(N);
-      int perNum=PerfectNum(N);
-      if(PalinNum==0 && AmsNum==0 && perNum==0){
+      int n=sc.nextInt();
+      int palindromenum=palindrome(n);
+      int amstrongnum=amstrong(n);
+      int perfectnum=perfectNum(n);
+      if(palindromenum==0 && amstrongnum==0 && perfectnum==0){
     	  System.out.print("None of these");
       }
 	}
 
-	private static int PerfectNum(int n) {
+	private static int perfectNum(int n) {
 		// TODO Auto-generated method stub
-		int Ans=0;
+		int ans=0;
 		
 		for(int i=1;i<=n/2;i++) {
 			if(n%i==0) {
-				Ans+=i;
+				ans+=i;
 			}
 		}
-		if(n==Ans) System.out.println("This no is perfect num");
-		else Ans=0;
-		return Ans;
+		if(n==ans) System.out.println("This no is perfect num");
+		else ans=0;
+		return ans;
 		
 	}
 
-	private static int Amstrong(int n) {
+	private static int amstrong(int n) {
 		// TODO Auto-generated method stub
-		int Ans=0;
+		int ans=0;
 		int b=n;
 		while(n>0) {
 			int a=n%10;
-			Ans+=Math.pow(a, 3);
+			ans+=Math.pow(a, 3);
 			n=n/10;
 		}
-		if(b==Ans) 
+		if(b==ans) 
 			System.out.println("This no is Amstrong");
-		else Ans=0;
-		return Ans;
+		else ans=0;
+		return ans;
 	}
 
-	private static int Palindrome(int n) {
+	private static int palindrome(int n) {
 		// TODO Auto-generated method stub
 		int b=n;
-		int Ans=0;
+		int ans=0;
 		while(n>0) {
 			int a=n%10;
-			Ans=Ans*10+a;
+			ans=ans*10+a;
 			n=n/10;
 		}
 		
-		if(b==Ans) 
+		if(b==ans) 
 			System.out.println("This no is palindrome");
-		else Ans=0;
-		return Ans;
+		else ans=0;
+		return ans;
 		
 	}
 
 }
+
--- DAY 1/Primitives.java
+++ DAY 1/Primitives.java
@@ -1,26 +1,27 @@
 package Day1Task;
 
-public class Primitives {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		byte val1=127;
-		short val2=32767;
-		int val3=2147483647;
-		long val4=9876543212345678l;
-		float val5=12345.0f;
-		double val6=98765432123456d;
-		char val7='a';
-		boolean val8=true;
-		System.out.println("Byte "+val1);
-		System.out.println("Short "+val2);
-		System.out.println("Int "+val3);
-		System.out.println("Long "+val4);
-		System.out.println("Float "+val5);
-		System.out.println("Double "+val6);
-		System.out.println("Char "+val7);
-		System.out.println("Boolean "+val8);
-
-	}
+public class Primitives{
+	static int intval;
+	static float floatval;
+	static byte byteval;
+	static short shortval;
+	static double doubleval;
+	static char charval;
+	static boolean booleanval;
+	static long longval;
+public static void main(String[] args) {
+   
+   System.out.println("Byte: "+intval);
+   System.out.println("Short: "+floatval);
+   System.out.println("Int: "+byteval);
+   System.out.println("Long: "+shortval);
+   System.out.println("Float: "+doubleval);
+   System.out.println("Double: "+charval);
+   System.out.println("Char: "+booleanval);
+   System.out.println("Boolean: "+longval);
 
 }
+
+}
+
+
